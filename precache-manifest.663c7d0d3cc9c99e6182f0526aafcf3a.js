947bf4a1ab87369110f5a174adfc996a436c8ae7
jonrhymes <jonrhymes@gmail.com>
2020-09-21 16:42:40 -0400

    Updates

---
+++ precache-manifest.663c7d0d3cc9c99e6182f0526aafcf3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c667271578d79fb4509b61d3d2946d",
+    "url": "/bookmarkd-frontend/index.html"
+  },
+  {
+    "revision": "43a46d73e4710ae44589",
+    "url": "/bookmarkd-frontend/static/css/main.f5027eb3.chunk.css"
+  },
+  {
+    "revision": "d3e8be66fbae0b017383",
+    "url": "/bookmarkd-frontend/static/js/2.87ff0b0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookmarkd-frontend/static/js/2.87ff0b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a46d73e4710ae44589",
+    "url": "/bookmarkd-frontend/static/js/main.a479923d.chunk.js"
+  },
+  {
+    "revision": "8149fb37ca56e8dbe931",
+    "url": "/bookmarkd-frontend/static/js/runtime-main.c464b92e.js"
+  }
+]);
